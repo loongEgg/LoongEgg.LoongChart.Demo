a48597af47aa79d1a84c6bc44db7150086be83c8
loongEgg <loongEgg@users.noreply.github.com>
2020-05-21 00:22:39 +0800

    [蛋兄]-65 依赖倒置原则，IoC容器实现依赖注入  WPF,C#

---
+++ LoongEgg.LoongChart.Demo/IoC/MasterWindowIoC.xaml.cs
@@ -0,0 +1,19 @@
+﻿using LoongEgg.MvvmCore.FX45;
+using System.Windows;
+
+namespace LoongEgg.LoongChart.Demo
+{
+    /// <summary>
+    /// MasterWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MasterWindowIoC : Window
+    {
+        public MasterWindowIoC()
+        {
+            InitializeComponent();
+            new SlaveWindowIoC().Show();
+        }
+
+        public ComunicationViewModel VM { get; private set; } = IoC.GetSingleton<ComunicationViewModel>(); 
+    }
+}
---
+++ LoongEgg.LoongChart.Demo/IoC/SlaveWindowIoC.xaml.cs
@@ -0,0 +1,18 @@
+﻿using LoongEgg.MvvmCore.FX45;
+using System.Windows;
+
+namespace LoongEgg.LoongChart.Demo
+{
+    /// <summary>
+    /// SlaveWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class SlaveWindowIoC : Window
+    {
+        public SlaveWindowIoC()
+        {
+            InitializeComponent();
+        }
+
+        public ComunicationViewModel VM { get; private set; } = IoC.GetSingleton<ComunicationViewModel>();
+    }
+}
